bd335976cd00e3310302154e610186271524eea0
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2025-03-01 14:29:11 +0600

    01-03-2025

--- python_for_beginner/chapter_1/exercise2.py
+++ python_for_beginner/chapter_1/exercise2.py
@@ -4,3 +4,14 @@ if len(user_name) <= 12 and " " not in user_name and not user_name.isdigit():
     print("Valid username")
 else:
     print("Invalid username")
+
+
+
+credit_card = input("Enter Your Credit Card Number: ")
+
+if all(char.isdigit() or char == "-" for char in credit_card) and "-" in credit_card:
+    last_four_digits = credit_card.replace("-", "")[-4:]
+
+    print(f"Last four digits: xxxx-xxxx-xxxx-{last_four_digits}")
+else:
+    print("Provide a valid Credit Card number (only digits and hyphens allowed).")
