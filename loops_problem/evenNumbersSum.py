f96bd37d24f7acb3dcbb419517bd406aefa96816
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2025-03-14 04:05:17 +0600

    loop practice

---
+++ loops_problem/evenNumbersSum.py
@@ -0,0 +1,8 @@
+n = 200
+sum_even = 0
+
+for i in range(1,n+1):
+    if i%2==0:
+        sum_even +=1
+
+print(f"Sum of Even Number: {sum_even} ")
---
+++ loops_problem/multiplicationTable.py
@@ -0,0 +1,7 @@
+n = 8
+
+for i in range(1,11):
+    if i == 5:
+       print (f"{i} is skipped!!")
+       continue
+    print(n*i)
