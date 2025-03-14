a92047ef0519c868f437c0fae027dcee74c22f3e
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2025-03-14 06:29:28 +0600

    loop practce

---
+++ loops_problem/nonRepeatedCharecter.py
@@ -0,0 +1,5 @@
+s = "teeter"
+
+for i in s:
+    if s.count(i) == 1:
+        print("Character is :",i)
---
+++ loops_problem/reverseString.py
@@ -0,0 +1,7 @@
+s = "good morning"
+
+reverse_str = ""
+
+for i in s:
+    reverse_str = i + reverse_str
+print(reverse_str)
