bc883a4841467d5d1ac0eda883185b6c9fd85a05
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2025-03-14 12:39:26 +0600

    attempts and wait time

--- loops_problem/exponentialBackoff.py
+++ loops_problem/exponentialBackoff.py
@@ -6,6 +6,8 @@ attempts = 0
 
 while attempts < max_retries:
   print ("Attempt", attempts +1," - wait time",wait_time)
+  #print (attempts)
   time.sleep(wait_time)
   wait_time *=2
   attempts +=1
+  #print (attempts)
