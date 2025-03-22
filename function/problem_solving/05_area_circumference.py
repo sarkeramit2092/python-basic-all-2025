d87d075bf140fec33ce83db4b3d9b91001416c39
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2025-03-22 21:16:28 +0600

    print(f"Area: {a:.2f}, Circumference: {c:.2f}")

--- function/problem_solving/05_area_circumference.py
+++ function/problem_solving/05_area_circumference.py
@@ -10,3 +10,19 @@ radius = int(input("Enter the radius: "))
 
 print(area(radius))
 print(circum(radius))
+
+def multi_return(radius):
+    area = (math.pi * (r**2))
+    circumference = (2 * math.pi * r)
+    return area, circumference
+
+a, c = multi_return(3)
+print("Area: ", a, "Circumference: ", c)
+
+def multi_return(radius):
+    area = math.pi * (radius**2)
+    circumference = 2 * math.pi * radius
+    return area, circumference
+
+a, c = multi_return(3)
+print(f"Area: {a:.2f}, Circumference: {c:.2f}")
